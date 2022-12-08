65b00c00d6f4a4c17d6cb665ba5d2ceb2eaaea92
NeverAlone1945 <agungsetiabudi1945@gmail.com>
2022-12-08 10:56:53 +0700

    add meta tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,15 @@ import Head from "next/head";
 export default function Header() {
   return (
     <Head>
-      <title>Terra Express</title>
-      <meta name="description" content="Software Management Ekspedisi" />
+      <title>Terra Express - Software Ekspedisi Terjangkau</title>
+      <meta
+        name="description"
+        content="Butuh software untuk menajemen usaha ekspedisi anda? Terra Express hadir untuk memenuhi kebutuhan anda. Dengan harga yang terjangkau, usaha anda akan lebih produktif dan tetap efisien"
+      />
+      <meta
+        name="keywords"
+        content="software ekspedisi, software logistik, aplikasi ekspedisi, aplikasi logistik, aplikasi pengiriman barang, aplikasi tracking pengiriman, software, aplikasi, ekspedisi, logistik"
+      />
       <link rel="icon" href="/image/icon-bw.png" />
     </Head>
   );
